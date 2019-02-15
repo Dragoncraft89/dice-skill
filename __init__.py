a599a9c4d2719abe80f7e91fddf53bdda62dfaff
florian <floriankothmeier@web.de>
2019-02-15 11:14:16 +0100

    Fix for non en-us languages

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
+from mycroft.util.parse import extract_number
 
 import random
 import re
@@ -14,16 +15,16 @@ class DiceSkill(MycroftSkill):
     def handle_roll_dice_intent(self, message):
         utterance = message.data['utterance']
         dice_type = message.data['DiceType']
-        utterance = utterance.replace('d' + dice_type, '')
+        utterance = utterance.replace(dice_type, '')
         dice_range = int(re.sub("[^0-9]", "", dice_type))
         dice_count = extract_number(utterance) or 1
-        self.roll(dice_count, dice_range)
+        self.roll(int(dice_count), dice_range)
 
     @intent_handler(IntentBuilder("").require("Roll").require("Dice"))
     def handle_roll_normal_intent(self, message):
         utterance = message.data['utterance']
         dice_count = extract_number(utterance) or 1
-        self.roll(dice_count, 6)
+        self.roll(int(dice_count), 6)
     
     def roll(self, dice_count, dice_range):
         numbers = [random.randint(1, dice_range) for i in range(dice_count)]
@@ -33,6 +34,7 @@ class DiceSkill(MycroftSkill):
         else:
             self.speak_dialog("multiple.result", data={"result" : ', '.join([str(x) for x in numbers])})
 
+"""
     @intent_handler(IntentBuilder("").require("Number").optionally("Range"))
     def handle_generate_number_intent(self, message):
         range_input = message.data.get('Range')
@@ -51,6 +53,7 @@ class DiceSkill(MycroftSkill):
         if low > high:
             low, high = high, low
         self.speak_dialog("single.result", data={"result" : random.randint(low, high)})
+"""
 
 def create_skill():
     return DiceSkill()
