293462ae43e3c41f1cd5646006904706fccef416
Mike Gray <mike@graywind.org>
2023-10-14 22:05:27 -0500

    feat: migrate to ovos

--- __init__.py
+++ __init__.py
@@ -1,64 +1,54 @@
-# TODO: Remove unused OVOS imports
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,redefined-outer-name
+import random
+
 from ovos_workshop.decorators import intent_handler
 from ovos_workshop.skills import OVOSSkill
-from ovos_utils.intents import IntentBuilder
-from ovos_bus_client.message import Message
-from adapt.intent import IntentBuilder
-# TODO: Remove all Mycroft imports
-# from mycroft.skills.core import MycroftSkill, intent_handler, intent_file_handler
-# TODO: Remove all Mycroft imports
-# from mycroft.util.log import LOG
-# TODO: Remove all Mycroft imports
-# from mycroft.util.parse import extract_number, normalize
+from lingua_franca.parse import extract_number
 
-import random
-import re
 
 class DiceSkill(OVOSSkill):
+    def __init__(self, *args, **kwargs):
+        super(*args, **kwargs).__init__()
 
-    def __init__(self):
-        super(DiceSkill, self).__init__(name="DiceSkill")
-
-    @intent_file_handler("Dice.intent")
+    @intent_handler("Dice.intent")
     def handle_roll_dice_intent(self, message):
-        dice_count = message.data.get('count') or "1"
+        dice_count = message.data.get("count") or "1"
         dice_count = int(extract_number(dice_count))
-        dice_type = message.data.get('type') or "6"
-        modificator = message.data.get('modifier')
+        dice_type = message.data.get("type") or "6"
+        modificator = message.data.get("modifier")
         if modificator:
             modificator = int(extract_number(modificator))
 
         self.roll(dice_count, int(dice_type), modificator)
 
-    @intent_file_handler("NormalDice.intent")
+    @intent_handler("NormalDice.intent")
     def handle_roll_normal_intent(self, message):
-        dice_count = message.data.get('count') or "1"
+        dice_count = message.data.get("count") or "1"
         dice_count = int(extract_number(dice_count))
         self.roll(dice_count, 6, None)
-            
+
     def roll(self, dice_count, dice_range, modificator):
         numbers = [random.randint(1, dice_range) for i in range(dice_count)]
         if modificator:
             total = sum(numbers) + modificator
             if len(numbers) == 1:
-                self.speak_dialog("single.modificator.result" , data={"result" : numbers[0], "total" : total})
+                self.speak_dialog("single.modificator.result", data={"result": numbers[0], "total": total})
             else:
-                self.speak_dialog("multiple.modificator.result", data={"result" : ', '.join([str(x) for x in numbers]), "total": total})
+                self.speak_dialog(
+                    "multiple.modificator.result",
+                    data={"result": ", ".join([str(x) for x in numbers]), "total": total},
+                )
         else:
             if len(numbers) == 1:
-                self.speak_dialog("single.result", data={"result" : numbers[0]})
+                self.speak_dialog("single.result", data={"result": numbers[0]})
             else:
-                self.speak_dialog("multiple.result", data={"result" : ', '.join([str(x) for x in numbers])})
+                self.speak_dialog("multiple.result", data={"result": ", ".join([str(x) for x in numbers])})
 
-    @intent_file_handler("Range.intent")
+    @intent_handler("Range.intent")
     def handle_generate_number_intent(self, message):
-        low = extract_number(message.data.get('low'))
-        high = extract_number(message.data.get('high'))
-        
+        low = extract_number(message.data.get("low"))
+        high = extract_number(message.data.get("high"))
+
         if low > high:
             low, high = high, low
-        self.speak_dialog("single.result", data={"result" : random.randint(low, high)})
-
-# TODO: Remove create_skill() function
-def create_skill():
-    return DiceSkill()
+        self.speak_dialog("single.result", data={"result": random.randint(low, high)})
